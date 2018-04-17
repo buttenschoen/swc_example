5fd6231df4266869c68f9125279d6ca09d4ccbf8
Birger Buttenschön <birger.buttenschoen@googlemail.com>
2018-04-17 14:17:04 +0200

    added argument parsing and weight conversion

--- newton/app.py
+++ newton/app.py
@@ -1,2 +1,20 @@
+import argparse
+
+def mass_to_weight(mass):
+	g = 9.81
+	weight = mass * g
+	return weight
+
 def main():
-	print("My name is newton")
+	parser = argparse.ArgumentParser(
+		prog='newton',
+		description='mass to weight converter')
+	parser.add_argument(
+		'--mass', '-m',
+		type=float,
+		help='Mass in kg',
+		required=True)
+	args = parser.parse_args()
+	print("Mass is %s kg" % args.mass)
+	weight = mass_to_weight(args.mass)
+	print("Weight is %s N" % weight)
